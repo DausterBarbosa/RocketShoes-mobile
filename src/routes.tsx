2aa908845a577f03493a2b74bc74441b43945ff9
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-09-21 14:54:45 -0300

    rotas configuradas

---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import {createStackNavigator} from "@react-navigation/stack";
+
+import Home from "./pages/Cart";
+import Cart from "./pages/Home";
+
+const Stack = createStackNavigator();
+
+function Routes(){
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="Cart" component={Cart}/>
+        </Stack.Navigator>
+    );
+}
+
+export default Routes;
